b2778f0ff78caf9c8af99cb015c0de072b0b8011
UYWGPvpS <o.chabdaev@gmail.com>
2021-12-15 14:26:04 +0200

    Added test database

--- config.py
+++ config.py
@@ -6,9 +6,16 @@ user = os.environ.get('MYSQL_USER')
 password = os.environ.get('MYSQL_PASSWORD')
 server = os.environ.get('MYSQL_SERVER')
 database = os.environ.get('MYSQL_DATABASE')
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-class Config:
+class Config(object):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{user}:{password}@{server}/{database}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestingConfig(object):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'department_app', 'tests', 'test.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- utest_app.py
+++ utest_app.py
@@ -1,10 +1,25 @@
+"""
+Initializes tests.
+"""
 import unittest
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from config import TestingConfig
 from department_app.tests.department import DepartmentModelTests
 from department_app.tests.department_service import DepartmentServiceTests
 from department_app.tests.employee import EmployeeModelTests
 from department_app.tests.employee_service import EmployeeServiceTests
 
+
 if __name__ == '__main__':
+    app = Flask(__name__)
+    app.debug = True
+    app.config['SECRET_KEY'] = 'SECRET_KEY'
+    app.config.from_object(TestingConfig)
+    api = Api(app)
+    db = SQLAlchemy(app)
+
     appTestSuite = unittest.TestSuite()
     appTestSuite.addTest(unittest.makeSuite(DepartmentModelTests))
     appTestSuite.addTest(unittest.makeSuite(DepartmentServiceTests))
